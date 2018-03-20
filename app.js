0f5ab917cb90bc4e1f0cf4290da6a4bb25469d46
Chrisgraham0104 <chrisgraham0104@gmail.com>
2018-03-20 13:12:58 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,113 +1,70 @@
-
-var restify = require('restify');
-var builder = require('botbuilder');
-
-console.log (process.env);
-// Setup Restify Server
-var server = restify.createServer();
-
-var header = {'Content-Type':'application/json', 
-'Ocp-Apim-Subscription-Key':'6fb7959510a84e389985fd3343705e6b'};
-
-console.log (server);
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+var restify = require('restify'); 
+var builder = require('botbuilder');  
+// Setup Restify Server 
+var server = restify.createServer(); 
+server.listen(process.env.port || process.env.PORT || 3978, 
+function () {    
+    console.log('%s listening to %s', server.name, server.url);  
+});  
+// chat connector for communicating with the Bot Framework Service 
+var connector = new builder.ChatConnector({     
+    appId: process.env.MICROSOFT_APP_ID,     
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
+// Listen for messages from users  
+server.post('/api/messages', connector.listen());  
 
-// Create chat connector for communicating with the Bot Framework Service
-var connector = new builder.ChatConnector({
-    appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword
-});
+var inMemoryStorage = new builder.MemoryBotStorage();
 
-// Listen for messages from users 
-server.post('/api/messages', connector.listen());
+// This is a dinner reservation bot that uses multiple dialogs to prompt users for input.
+var bot = new builder.UniversalBot(connector, [
+    function (session) {
+        session.send("Welcome to the dinner reservation.");
+        session.beginDialog('askForDateTime');
+    },
+    function (session, results) {
+        session.dialogData.reservationDate = builder.EntityRecognizer.resolveTime([results.response]);
+        session.beginDialog('askForPartySize');
+    },
+    function (session, results) {
+        session.dialogData.partySize = results.response;
+        session.beginDialog('askForReserverName');
+    },
+    function (session, results) {
+        session.dialogData.reservationName = results.response;
 
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-    //session.send("You said: %s", session.message.text);
-    console.log("hi");
-    bot.dialog('/happy', [    
-        function(session) {    
-            console.log('1');
-        builder.Prompts.text(session, "That's awesome! What would make you even happier?");    
-        },    
-        function(session, results) { 
-            console.log('2');
-             getGiphy(results.response).then(function(gif) {  
-              session.send(gif.toString());      
-             console.log('console data -->' + JSON.parse(gif).data);   
-             session.send({                
-                text: "Here you go!",                
-                attachments: [                    
-                   {                        
-                      contentType: 'image/gif',     
-                      contentUrl: 
-                        JSON.parse(gif).data.images.original.url  
-                   }                
-                ]            
-             });        
-        }).catch(function(err) {            
-              console.log("Error getting giphy: " + err);  
-              session.send({                
-                 text: "We couldn't find that unfortunately :(",
-                 attachments: [                    
-                   {                        
-                      contentType: 'image/gif',  
-                      contentUrl: 'https://media.giphy.com/media/ToMjGpt4q1nF76cJP9K/giphy.gif',  
-                      name: 'Chicken nugz are life'   
-                   }                
-                 ]            
-               });        
-           }).then(function(idk) {     
-              builder.Prompts.text(session, "Would you like to see more?");        
-           });    
-        },    
-        function (session, results) {        
-            console.log('3');
-           if (results.response === "Yes" || results.response ===
-           "yes") {            
-             session.beginDialog('/giphy');        
-           } else {            
-             session.endDialog("Have a great rest of your day!!!");           
-          }    
-        }
-     ]);
-});
-
-bot.on('conversationUpdate', function (message) {    
-    if (message.membersAdded[0].id === message.address.bot.id) {             
-          var reply = new builder.Message()    
-                .address(message.address)    
-                .text("Hello, I'm careBOTyou! How's your day going?");        
-          bot.send(reply);    
+        // Process request and display reservation details
+        session.send(`Reservation confirmed. Reservation details: <br/>Date/Time: ${session.dialogData.reservationDate} <br/>Party size: ${session.dialogData.partySize} <br/>Reservation name: ${session.dialogData.reservationName}`);
+        session.endDialog();
     }
- }); 
+]); // Register in-memory storage 
 
+// Dialog to ask for a date and time
+bot.dialog('askForDateTime', [
+    function (session) {
+        builder.Prompts.time(session, "Please provide a reservation date and time (e.g.: June 6th at 5pm)");
+    },
+    function (session, results) {
+        session.endDialogWithResult(results);
+    }
+]);
 
+// Dialog to ask for number of people in the party
+bot.dialog('askForPartySize', [
+    function (session) {
+        builder.Prompts.text(session, "How many people are in your party?");
+    },
+    function (session, results) {
+        session.endDialogWithResult(results);
+    }
+])
 
-
-
-function sendGetSentimentRequest(message) {      
-   var options = {        
-      method: 'POST',        
-      uri:'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment',        
-      body: {            
-         documents:[{id:'1', language: 'en', text:message}]  
-      },        
-      json: true, // Automatically stringifies the body to JSON
-      headers: header    
-   };    
-   return rp(options)
-;}
-function getGiphy(searchString) {    
-   var options = {        
-      method: 'GET',        
-      uri: 'https://api.giphy.com/v1/gifs/translate',        
-      qs: {            
-         s: searchString,            
-         api_key: '9n8AIaWULVu37sA1k8eE38IwnCCjmXP9'        
-      }    
-   }    
-   return rp(options)
-;}
+// Dialog to ask for the reservation name.
+bot.dialog('askForReserverName', [
+    function (session) {
+        builder.Prompts.text(session, "Who's name will this reservation be under?");
+    },
+    function (session, results) {
+        session.endDialogWithResult(results);
+    }
+]);
